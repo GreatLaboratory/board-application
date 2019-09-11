2d4b6911847e60c941971e76c16ded5208ee3afb
Myung-gwan Kim <cartopia95@naver.com>
2019-09-11 21:24:24 +0900

    헤더 (로그인 여부 판단)

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="app-header">
+    <h1>Community</h1>
+    <div v-if="isAuthorized">
+      <strong>
+        <button @click="toggle">{{ me.name }}님 환영합니다.</button>
+        <i v-if="!isActive" class="fas fa-sort-down"></i>
+        <i v-else class="fas fa-sort-up"></i>
+      </strong>
+      <ul v-if="isActive">
+        <li><button @click="onClickSignout">로그아웃</button></li>
+      </ul>
+    </div>
+    <div v-else>
+      <router-link :to="{name: 'SigninPage'}">로그인</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapGetters, mapState, mapActions } from "vuex"
+
+  export default {
+    name: "AppHeader",
+    computed: {
+      ...mapGetters([
+        "isAuthorized"
+      ]),
+      ...mapState([
+        "me"
+      ])
+    },
+    data() {
+      return {
+        isActive : false
+      }
+    },
+    methods: {
+      ...mapActions([
+        "signout"
+      ]),
+      onClickSignout(){
+        this.signout();
+        this.$router.push({ name: "PostListPage" })
+      },
+      toggle() {
+        this.isActive = !this.isActive;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
