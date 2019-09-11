cfb04d8ca731af62736705562c856ac8eb19ae7c
Myung-gwan Kim <cartopia95@naver.com>
2019-09-11 21:25:46 +0900

    게시물 작성하기 페이지 - 컴포넌트

---
+++ src/components/PostCreateForm.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <fieldset>
+      <label>제목</label>
+      <input v-model="title" type="text" placeholder="게시물 제목을 입력해주세요."/>
+      <label>내용</label>
+      <textarea v-model="contents" rows="5" placeholder="게시물 내용을 입력해주세요."></textarea>
+      <button type="submit">게시하기</button>
+    </fieldset>
+  </form>
+</template>
+
+<script>
+    export default {
+      name: "PostCreateForm",   // 이걸로 부모 컴포넌트의 커스텀태그가 결정되므로 잘 설정해야한다.
+      data() {
+        return {
+          title: "",
+          contents: ""
+        }
+      },
+      methods: {
+        onSubmit() {
+          const { title, contents } = this;
+          this.$emit("submit", { title, contents });
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/PostCreatePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="post-create-page">
+    <h3>게시물 작성하기</h3>
+    <post-create-form @submit="onSubmit"/>
+  </div>
+</template>
+
+<script>
+    import PostCreateForm from "@/components/PostCreateForm";
+    export default {
+      name: "PostCreatePage",
+      components: { PostCreateForm },
+      methods: {
+        onSubmit(payload) {
+          // api연동 전 // 여기서부터 다시 시작해야함
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
