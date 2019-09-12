81724d88b4129d78836fc66994fd38a487e2f1b1
Myung-gwan Kim <cartopia95@naver.com>
2019-09-12 20:49:13 +0900

    라우터에 게시물 수정, 등록에 대한 권한 컴포넌트 가드 설정

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import PostCreatePage from "@/pages/PostCreatePage";
 
 Vue.use(Router);
 
+import store from "@/store"
+import PostEditPage from "@/pages/PostEditPage";
+
 export default new Router({
   mode: "history",
   routes: [
@@ -18,6 +21,14 @@ export default new Router({
       components: {
         header: AppHeader,
         default: PostCreatePage
+      },
+      beforeEnter (to, from, next) {
+        const { isAuthorized } = store.getters;
+        if (!isAuthorized) {
+          alert("로그인이 필요합니다.");
+          next({ name: "SigninPage" });
+        }
+        next();
       }
     },
     {
@@ -38,6 +49,42 @@ export default new Router({
       props: {
         default: true   // 이 라우터의 파라미터로 할당되어 있는 postId값을 PostViewPage컴포넌트로 가져가야한다.
                         // 그래서 이렇게 props를 true로 해놓으면 자식개념인 PostViewPage컴포넌트가 postId를 가져다 쓸 수 있다.
+      }
+    },
+    {
+      path: "/post/:postId/edit",
+      name: "PostEditPage",
+      components: {
+        header: AppHeader,
+        default: PostEditPage
+      },
+      props: {
+        default: true
+      },
+      beforeEnter(to, from, next) {
+        const { isAuthorized } = store.getters;
+        if (!isAuthorized) {
+          alert("로그인이 필요합니다.");
+          next({ name: "SigninPage" });
+          return false;  // 여기선 왜 이 코드를 넣었을까.. // 아 아래 store.dispatch 실행시키지않으려고 빠져나오는 코드구나
+        }
+        store.dispatch("fetchPost", to.params.postId)
+          .then(()=>{
+            // 먼저 이 글을 쓴 사람이 현재 로그인으로 인증받은 사람인지를 판별한다.
+            const post = store.state.post;
+            const isAuthor = post.user.id === store.state.me.id;
+            if (isAuthor) {
+              // 일치하면서 게시물 데이터 요청이 성공했다면 다음 라우팅을 그대로 진행한다.
+              next();
+            } else {
+              alert("게시물의 작성자만 게시물을 수정할 수 있습니다.");
+              next(from)
+            }
+          })
+          .catch((err)=>{
+            alert(err.response.data.msg);
+            next(from)
+          })
       }
     },
     {
