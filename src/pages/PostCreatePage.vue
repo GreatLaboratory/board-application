f8fd2fc663508505ab67af538c714338fc870b24
Myung-gwan Kim <cartopia95@naver.com>
2019-09-12 20:46:44 +0900

    게시물 등록하기 api등록 및 에러 핸들링

--- src/pages/PostCreatePage.vue
+++ src/pages/PostCreatePage.vue
@@ -7,12 +7,31 @@
 
 <script>
     import PostCreateForm from "@/components/PostCreateForm";
+    import api from "@/api"
+
     export default {
       name: "PostCreatePage",
       components: { PostCreateForm },
       methods: {
+        // 여기 메소드는 액션함수로 끌어오지않는데 이유는 컴포넌트들이 공통으로 관리하는 store관리가 필요없기 때문이다.
         onSubmit(payload) {
-          // api연동 전 // 여기서부터 다시 시작해야함
+          const { title, contents } = payload;
+          api.post("/posts", { title, contents })
+            .then((res)=>{
+              alert("게시물이 성공적으로 작성되었습니다.");
+              this.$router.push({
+                name: "PostViewPage",
+                params: { postId: res.data.id.toString() }
+              })
+            })
+            .catch((err)=>{
+              if (err.response.status === 401) {
+                alert("로그인이 필요합니다.");
+                this.$router.push({ name : "SigninPage" });
+              } else {
+                alert(err.response.data.msg);  // 서버에서 응답하는 에러메세지 노출
+              }
+            });
         }
       }
     }
